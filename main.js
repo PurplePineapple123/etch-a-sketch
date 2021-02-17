a522d480cbc67bf5e83476799f8ba3c758cfaf62
Philip Pages <philip.pages@gmail.com>
2021-02-17 08:02:36 -0600

    black and psych buttons working

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
+//Generate grid to size of user input
 
-        
 function createGrid(widthHeight){
     document.getElementById(`grid-container`).innerHTML = "";
 
@@ -12,9 +12,23 @@ function createGrid(widthHeight){
         for (let y = 0; y < widthHeight; y++){
             let divCol = document.createElement(`div`);
             divCol.classList.add(`divCol`);
-            divCol.addEventListener("mouseenter", function(){
-                this.style.backgroundColor = `red`;
-            })
+
+            //add color changing functions here
+
+            //black button
+            document.getElementById(`black-button`).addEventListener(`click`, function() {
+                divCol.addEventListener("mouseenter", function(){
+                    this.style.backgroundColor = `black`;
+                });
+            });
+            
+            //psychedlic button selection
+            document.getElementById(`psych-button`).addEventListener(`click`, function() {
+                divCol.addEventListener("mouseenter", function(){
+                    this.style.backgroundColor = `#${randomColor()}`;
+                });
+            });
+
             divRow.appendChild(divCol);
         }
     }
@@ -22,4 +36,11 @@ function createGrid(widthHeight){
 
 }
 
-createGrid(75);
+createGrid(7);
+
+//returns random colors for psychedleic button
+function randomColor() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor);
+    return randomColor;
+}
