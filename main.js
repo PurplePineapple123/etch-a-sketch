7de1f275fe83d1554b34a4c62c7a5c224d8dac81
Philip Pages <philip.pages@gmail.com>
2021-02-17 08:30:30 -0600

    added erase button

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ function createGrid(widthHeight){
 
             //add color changing functions here
 
+            //default hover
+            divCol.addEventListener("mouseenter", function(){
+                this.style.backgroundColor = `black`;
+            });
+
             //black button
             document.getElementById(`black-button`).addEventListener(`click`, function() {
                 divCol.addEventListener("mouseenter", function(){
@@ -29,12 +34,40 @@ function createGrid(widthHeight){
                 });
             });
 
+            //eraser button
+
+            document.getElementById(`erase-button`).addEventListener(`click`, function() {
+                divCol.addEventListener("mouseenter", function(){
+                    this.style.backgroundColor = `rgb(156, 149, 136)`;
+                });
+            });
+
             divRow.appendChild(divCol);
         }
     }
 
 
 }
+
+// slider.oninput = function changeGridSize() {
+//     let sliderValue = document.getElementById(`myRange`);
+//     sliderValue = sliderValue.value;
+//     return(sliderValue);
+// }
+
+
+// let slider = document.getElementById(`myRange`);
+
+// slider.oninput = function() {
+//     createGrid(this.value);
+// }
+
+// document.getElementById(`myRange`).addEventListener(`input`, function(){
+//     createGrid(this.value);
+// });
+
+
+
 
 createGrid(7);
 
